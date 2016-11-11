bc6529ceb43460ab4d3a5674b702f9641ed441f6
Greg Bowler <greg.bowler@g105b.com>
2016-11-11 08:30:05 +0000

    Set defaults from class constants.

--- src/Connection/DefaultSettings.php
+++ src/Connection/DefaultSettings.php
@@ -3,24 +3,30 @@ namespace Gt\Database\Connection;
 
 class DefaultSettings {
 
+const DEFAULT_DATASOURCE = "mysql";
+const DEFAULT_DATABASE = "example";
+const DEFAULT_HOSTNAME = "localhost";
+const DEFAULT_USERNAME = "admin";
+const DEFAULT_PASSWORD = "";
+
 public function getDataSource():string {
-	return "";
+	return self::DEFAULT_DATASOURCE;
 }
 
 public function getDatabase():string {
-	return "";
+	return self::DEFAULT_DATABASE;
 }
 
 public function getHostname():string {
-	return "";
+	return self::DEFAULT_HOSTNAME;
 }
 
 public function getUsername():string {
-	return "";
+	return self::DEFAULT_USERNAME;
 }
 
 public function getPassword():string {
-	return "";
+	return self::DEFAULT_PASSWORD;
 }
 
 }#
