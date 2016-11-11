bd8847c123f450bae136db4f98b13bd85b9bd8f6
Greg Bowler <greg.bowler@g105b.com>
2016-11-11 08:27:14 +0000

    Test properties are set on Settings object.

--- test/unit/Connection/Settings.test.php
+++ test/unit/Connection/Settings.test.php
@@ -3,4 +3,28 @@ namespace Gt\Database\Connection;
 
 class SettingsTest extends \PHPUnit_Framework_TestCase {
 
+public function testPropertiesSet() {
+	$details = [
+		"dataSource" => "test-data-source",
+		"database" => "test-database",
+		"hostname" => "test-hostname",
+		"username" => "test-username",
+		"password" => "test-password",
+	];
+
+	$settings = new Settings(
+		$details["dataSource"],
+		$details["database"],
+		$details["hostname"],
+		$details["username"],
+		$details["password"]
+	);
+
+	$this->assertEquals($details["dataSource"], $settings->getDataSource());
+	$this->assertEquals($details["database"], $settings->getDatabase());
+	$this->assertEquals($details["hostname"], $settings->getHostname());
+	$this->assertEquals($details["username"], $settings->getUsername());
+	$this->assertEquals($details["password"], $settings->getPassword());
+}
+
 }#
