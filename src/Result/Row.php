7bd31e3ba806976554addd14f9907c5595ff4a47
Greg Bowler <greg.bowler@g105b.com>
2016-12-02 09:09:34 +0000

    Make Row iterable again.

--- src/Result/Row.php
+++ src/Result/Row.php
@@ -2,12 +2,20 @@
 namespace Gt\Database\Result;
 
 use ArrayAccess;
+use Iterator;
 
-class Row implements ArrayAccess {
+class Row implements ArrayAccess, Iterator {
+
+/** @var array */
+private $data;
+/** @var int */
+private $iteratorIndex = 0;
 
 public function __construct(array $data = []) {
 	$this->data = $data;
 }
+
+// ArrayAccess /////////////////////////////////////////////////////////////////
 
 public function offsetGet($offset) {
 	return $this->data[$offset];
@@ -23,6 +31,24 @@ public function offsetUnset($offset) {
 
 public function offsetExists($offset) {
 	return isset($this->data[$offset]);
+}
+
+// Iterator ////////////////////////////////////////////////////////////////////
+
+public function current() {
+	return $this->data[$this->iteratorIndex];
+}
+public function key() {
+	return $this->iteratorIndex;
+}
+public function next() {
+	++ $this->iteratorIndex;
+}
+public function rewind() {
+	$this->iteratorIndex = 0;
+}
+public function valid() {
+	return isset($this->data[$this->iteratorIndex]);
 }
 
 }#
