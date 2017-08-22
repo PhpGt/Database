eda75c6915a93f80bb6eff4c15267f1226e2f0e8
Greg Bowler <greg.bowler@g105b.com>
2017-08-22 17:04:37 +0100

    Remove unused import

--- test/unit/Connection/DefaultSettings.test.php
+++ test/unit/Connection/DefaultSettings.test.php
@@ -1,8 +1,6 @@
 <?php
 namespace Gt\Database\Connection;
 
-use Gt\Database\Connection\SettingsInterface;
-
 class DefaultSettingsTest extends \PHPUnit_Framework_TestCase {
 
 public function testImplementation() {
