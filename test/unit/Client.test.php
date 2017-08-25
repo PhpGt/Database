b77dd7a1dcc35df7134f2ea36ebc7020dbf00cbe
Greg Bowler <greg.bowler@g105b.com>
2017-08-25 12:02:13 +0100

    Pass Client tests

--- test/unit/Client.test.php
+++ test/unit/Client.test.php
@@ -23,9 +23,7 @@ public function testQueryCollectionPathExists(string $name, string $path) {
 	);
 	$db = new Client($settings);
 
-	static::assertTrue(isset($db[$name]));
 	$queryCollection = $db->queryCollection($name);
-
 	static::assertInstanceOf(QueryCollection::class, $queryCollection);
 }
 
@@ -42,45 +40,7 @@ public function testQueryCollectionPathNotExists(string $name, string $path) {
 		Settings::DATABASE_IN_MEMORY
 	);
 	$db = new Client($settings);
-	static::assertFalse(isset($db[$name]));
-
 	$db->queryCollection($name);
-}
-
-/**
- * @dataProvider \Gt\Database\Test\Helper::queryCollectionPathExistsProvider
- */
-public function testOffsetGet(string $name, string $path) {
-	$settings = new Settings(
-		dirname($path),
-		Settings::DRIVER_SQLITE,
-		Settings::DATABASE_IN_MEMORY
-	);
-	$db = new Client($settings);
-
-	$offsetGot = $db->offsetGet($name);
-	$arrayAccessed = $db[$name];
-
-	static::assertEquals(
-		$offsetGot->getDirectoryPath(),
-		$arrayAccessed->getDirectoryPath()
-	);
-}
-
-/**
- * @expectedException \Gt\Database\ReadOnlyArrayAccessException
- */
-public function testOffsetSet() {
-	$db = new Client();
-	$db["test"] = "qwerty";
-}
-
-/**
- * @expectedException \Gt\Database\ReadOnlyArrayAccessException
- */
-public function testOffsetUnset() {
-	$db = new Client();
-	unset($db["test"]);
 }
 
 }#
