7c185d90ba76967979b6e8c2f15687262936fdab
Greg Bowler <greg.bowler@g105b.com>
2017-12-05 17:08:38 +0000

    Match styleguide: Connection tests

--- test/unit/Connection/DefaultSettingsTest.php
+++ test/unit/Connection/DefaultSettingsTest.php
@@ -4,72 +4,70 @@ namespace Gt\Database\Connection;
 use PHPUnit\Framework\TestCase;
 
 class DefaultSettingsTest extends TestCase {
+	public function testImplementation() {
+		$settings = new DefaultSettings();
+		static::assertInstanceOf(SettingsInterface::class, $settings);
+	}
 
-public function testImplementation() {
-	$settings = new DefaultSettings();
-	static::assertInstanceOf(SettingsInterface::class, $settings);
-}
+	public function testDefaults() {
+		$settings = new DefaultSettings();
+		static::assertEquals(
+			DefaultSettings::DEFAULT_DATASOURCE,
+			$settings->getDataSource()
+		);
+		static::assertEquals(
+			DefaultSettings::DEFAULT_SCHEMA,
+			$settings->getSchema()
+		);
+		static::assertEquals(
+			DefaultSettings::DEFAULT_PORT[DefaultSettings::DEFAULT_DATASOURCE],
+			$settings->getPort()
+		);
+		static::assertEquals(
+			DefaultSettings::DEFAULT_HOST,
+			$settings->getHost()
+		);
+		static::assertEquals(
+			DefaultSettings::DEFAULT_USERNAME,
+			$settings->getUsername()
+		);
+		static::assertEquals(
+			DefaultSettings::DEFAULT_PASSWORD,
+			$settings->getPassword()
+		);
+	}
 
-public function testDefaults() {
-	$settings = new DefaultSettings();
-	static::assertEquals(
-		DefaultSettings::DEFAULT_DATASOURCE,
-		$settings->getDataSource()
-	);
-	static::assertEquals(
-		DefaultSettings::DEFAULT_SCHEMA,
-		$settings->getSchema()
-	);
-	static::assertEquals(
-		DefaultSettings::DEFAULT_PORT[DefaultSettings::DEFAULT_DATASOURCE],
-		$settings->getPort()
-	);
-	static::assertEquals(
-		DefaultSettings::DEFAULT_HOST,
-		$settings->getHost()
-	);
-	static::assertEquals(
-		DefaultSettings::DEFAULT_USERNAME,
-		$settings->getUsername()
-	);
-	static::assertEquals(
-		DefaultSettings::DEFAULT_PASSWORD,
-		$settings->getPassword()
-	);
-}
-
-/** @dataProvider getDataSources */
-public function testDefaultPort(string $dsn, int $port) {
+	/** @dataProvider getDataSources */
+	public function testDefaultPort(string $dsn, int $port) {
 // NOTE: Have to use a Settings object here as it's not possible to use anything other
 // than the default_datasource otherwise
-	$settings = new Settings(
-		"/tmp",
-		$dsn,
-		"test-database"
-	);
+		$settings = new Settings(
+			"/tmp",
+			$dsn,
+			"test-database"
+		);
 
-	static::assertEquals($port, $settings->getPort());
-	static::assertEquals([
-		"driver" => $dsn,
-		"host" => DefaultSettings::DEFAULT_HOST,
-		"port" => $port,
-		"schema" => "test-database",
-		"username" => DefaultSettings::DEFAULT_USERNAME,
-		"password" => DefaultSettings::DEFAULT_PASSWORD,
-		"charset" => DefaultSettings::CHARSET,
-		"collation" => DefaultSettings::COLLATION,
-		"prefix" => "",
-		"options" => DefaultSettings::DEFAULT_CONFIG["options"],
-	], $settings->getConnectionSettings());
+		static::assertEquals($port, $settings->getPort());
+		static::assertEquals([
+			"driver" => $dsn,
+			"host" => DefaultSettings::DEFAULT_HOST,
+			"port" => $port,
+			"schema" => "test-database",
+			"username" => DefaultSettings::DEFAULT_USERNAME,
+			"password" => DefaultSettings::DEFAULT_PASSWORD,
+			"charset" => DefaultSettings::CHARSET,
+			"collation" => DefaultSettings::COLLATION,
+			"prefix" => "",
+			"options" => DefaultSettings::DEFAULT_CONFIG["options"],
+		], $settings->getConnectionSettings());
+	}
+
+	public function getDataSources():array {
+		return [
+			[Settings::DRIVER_MYSQL, 3306],
+			[Settings::DRIVER_POSTGRES, 5432],
+			[Settings::DRIVER_SQLSERVER, 1433],
+			[Settings::DRIVER_SQLITE, 0],
+		];
+	}
 }
-
-public function getDataSources(): array {
-	return [
-		[ Settings::DRIVER_MYSQL, 3306 ],
-		[ Settings::DRIVER_POSTGRES, 5432],
-		[ Settings::DRIVER_SQLSERVER, 1433],
-		[ Settings::DRIVER_SQLITE, 0],
-	];
-}
-
-}#
