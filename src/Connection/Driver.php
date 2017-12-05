0469826d3a4f10952283c5daec4b1fb4284459b4
Greg Bowler <greg.bowler@g105b.com>
2017-12-05 17:03:41 +0000

    Match styleguide: Connection

--- src/Connection/Driver.php
+++ src/Connection/Driver.php
@@ -2,38 +2,36 @@
 namespace Gt\Database\Connection;
 
 class Driver {
+	/** @var SettingsInterface */
+	protected $settings;
+	/** @var Connection */
+	protected $connection;
 
-/** @var SettingsInterface */
-protected $settings;
-/** @var Connection */
-protected $connection;
+	public function __construct(SettingsInterface $settings) {
+		$this->settings = $settings;
+		$this->connect();
+	}
 
-public function __construct(SettingsInterface $settings) {
-	$this->settings = $settings;
-	$this->connect();
+	public function getBaseDirectory():string {
+		return $this->settings->getBaseDirectory();
+	}
+
+	public function getConnectionName():string {
+		return $this->settings->getConnectionName();
+	}
+
+	public function getConnection():Connection {
+		return $this->connection;
+	}
+
+	protected function connect() {
+		$options = null;
+
+		$this->connection = new Connection(
+			$this->settings->getConnectionString(),
+			$this->settings->getUsername(),
+			$this->settings->getPassword(),
+			$options
+		);
+	}
 }
-
-public function getBaseDirectory():string {
-	return $this->settings->getBaseDirectory();
-}
-
-public function getConnectionName():string {
-	return $this->settings->getConnectionName();
-}
-
-public function getConnection():Connection {
-	return $this->connection;
-}
-
-protected function connect() {
-	$options = null;
-
-	$this->connection = new Connection(
-		$this->settings->getConnectionString(),
-		$this->settings->getUsername(),
-		$this->settings->getPassword(),
-		$options
-	);
-}
-
-}#
--- src/Connection/SettingsInterface.php
+++ src/Connection/SettingsInterface.php
@@ -2,16 +2,23 @@
 namespace Gt\Database\Connection;
 
 interface SettingsInterface {
+	public function getBaseDirectory():string;
 
-public function getBaseDirectory():string;
-public function getDataSource():string;
-public function getSchema():string;
-public function getHost():string;
-public function getPort():int;
-public function getUsername():string;
-public function getPassword():string;
-public function getConnectionName():string;
-public function getTablePrefix():string;
-public function getConnectionString():string;
+	public function getDataSource():string;
 
-}#
+	public function getSchema():string;
+
+	public function getHost():string;
+
+	public function getPort():int;
+
+	public function getUsername():string;
+
+	public function getPassword():string;
+
+	public function getConnectionName():string;
+
+	public function getTablePrefix():string;
+
+	public function getConnectionString():string;
+}
