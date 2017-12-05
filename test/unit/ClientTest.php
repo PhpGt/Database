7306db148ca38cd99268af80d9af3b37b8e78559
Greg Bowler <greg.bowler@g105b.com>
2017-12-05 17:10:45 +0000

    Match styleguide: Base class tests

--- test/unit/ClientTest.php
+++ test/unit/ClientTest.php
@@ -6,42 +6,40 @@ use Gt\Database\Query\QueryCollection;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase {
+	public function testInterface() {
+		$db = new Client();
+		static::assertInstanceOf(Client::class, $db);
+	}
 
-public function testInterface() {
-	$db = new Client();
-	static::assertInstanceOf(Client::class, $db);
+	/**
+	 * @dataProvider \Gt\Database\Test\Helper\Helper::queryCollectionPathExistsProvider
+	 */
+	public function testQueryCollectionPathExists(string $name, string $path) {
+		$basePath = dirname($path);
+		$settings = new Settings(
+			$basePath,
+			Settings::DRIVER_SQLITE,
+			Settings::SCHEMA_IN_MEMORY
+		);
+		$db = new Client($settings);
+
+		$queryCollection = $db->queryCollection($name);
+		static::assertInstanceOf(QueryCollection::class, $queryCollection);
+	}
+
+	/**
+	 * @dataProvider \Gt\Database\Test\Helper\Helper::queryPathNotExistsProvider
+	 * @expectedException \Gt\Database\Query\QueryCollectionNotFoundException
+	 */
+	public function testQueryCollectionPathNotExists(string $name, string $path) {
+		$basePath = dirname($path);
+
+		$settings = new Settings(
+			$basePath,
+			Settings::DRIVER_SQLITE,
+			Settings::SCHEMA_IN_MEMORY
+		);
+		$db = new Client($settings);
+		$db->queryCollection($name);
+	}
 }
-
-/**
- * @dataProvider \Gt\Database\Test\Helper\Helper::queryCollectionPathExistsProvider
- */
-public function testQueryCollectionPathExists(string $name, string $path) {
-	$basePath = dirname($path);
-	$settings = new Settings(
-		$basePath,
-		Settings::DRIVER_SQLITE,
-		Settings::SCHEMA_IN_MEMORY
-	);
-	$db = new Client($settings);
-
-	$queryCollection = $db->queryCollection($name);
-	static::assertInstanceOf(QueryCollection::class, $queryCollection);
-}
-
-/**
- * @dataProvider \Gt\Database\Test\Helper\Helper::queryPathNotExistsProvider
- * @expectedException \Gt\Database\Query\QueryCollectionNotFoundException
- */
-public function testQueryCollectionPathNotExists(string $name, string $path) {
-	$basePath = dirname($path);
-
-	$settings = new Settings(
-		$basePath,
-		Settings::DRIVER_SQLITE,
-		Settings::SCHEMA_IN_MEMORY
-	);
-	$db = new Client($settings);
-	$db->queryCollection($name);
-}
-
-}#
