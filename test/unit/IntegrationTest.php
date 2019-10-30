e7ef45f0e257d6c799e1b94f12cc0e5a1286a549
Greg Bowler <greg.bowler@g105b.com>
2019-10-30 16:17:26 +0000

    Write tests for #115

--- test/unit/IntegrationTest.php
+++ test/unit/IntegrationTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Database;
 
+use DateTime;
 use Exception;
 use Gt\Database\Connection\Driver;
 use Gt\Database\Connection\Settings;
@@ -24,14 +25,14 @@ class IntegrationTest extends TestCase {
 		$driver = $this->db->getDriver();
 
 		$connection = $driver->getConnection();
-		$output = $connection->exec("CREATE TABLE test_table ( id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32), number integer, timestamp DATETIME DEFAULT current_timestamp); CREATE UNIQUE INDEX test_table_name_uindex ON test_table (name);");
+		$output = $connection->exec("CREATE TABLE test_table ( id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32), number integer, isEven bool, halfNumber float, timestamp DATETIME DEFAULT current_timestamp); CREATE UNIQUE INDEX test_table_name_uindex ON test_table (name);");
 
 		if($output === false) {
 			$error = $connection->errorInfo();
 			throw new Exception($error[2]);
 		}
 
-		$insertStatement = $connection->prepare("INSERT INTO test_table (name, number) VALUES ('one', 1), ('two', 2), ('three', 3)");
+		$insertStatement = $connection->prepare("INSERT INTO test_table (name, number, isEven, halfNumber) VALUES ('one', 1, false, 0.5), ('two', 2, true, 1), ('three', 3, false, 1.5)");
 		$success = $insertStatement->execute();
 		if($success === false) {
 			$error = $connection->errorInfo();
@@ -234,6 +235,181 @@ class IntegrationTest extends TestCase {
 		static::assertEquals(1, $result1->id);
 		static::assertEquals(2, $result2->id);
 		static::assertEquals(3, $result3->id);
+	}
+
+	public function testFetchBool() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/isNumberEven.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT isEven FROM test_table where number = ?"
+		);
+
+		$result1 = $this->db->fetchBool("exampleCollection/isNumberEven", 1);
+		$result2 = $this->db->fetchBool("exampleCollection/isNumberEven", 2);
+		self::assertFalse($result1);
+		self::assertTrue($result2);
+	}
+
+	public function testFetchAllBool() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getAllBools.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT isEven FROM test_table"
+		);
+
+		$result = $this->db->fetchAllBool("exampleCollection/getAllBools");
+		self::assertCount(3, $result);
+		self::assertIsArray($result);
+		foreach($result as $i => $value) {
+			if($i % 2 !== 0) {
+				self::assertTrue($value);
+			}
+			else {
+				self::assertFalse($value);
+			}
+		}
+	}
+
+
+	public function testFetchInt() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getIdByName.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT id FROM test_table WHERE name = :name LIMIT 1"
+		);
+
+		$result = $this->db->fetchInt("exampleCollection/getIdByName", "two");
+		self::assertEquals(2, $result);
+		self::assertIsInt($result);
+	}
+
+	public function testFetchAllInt() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getAllNumbers.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT number FROM test_table"
+		);
+
+		$result = $this->db->fetchAllInt("exampleCollection/getAllNumbers");
+		self::assertCount(3, $result);
+		self::assertIsArray($result);
+		foreach($result as $value) {
+			self::assertIsInt($value);
+		}
+	}
+
+
+	public function testFetchString() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getNameById.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT name FROM test_table WHERE id = ? LIMIT 1"
+		);
+
+		$result = $this->db->fetchString("exampleCollection/getNameById", 2);
+		self::assertEquals("two", $result);
+		self::assertIsString($result);
+	}
+
+	public function testFetchAllString() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getAllNames.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT name FROM test_table"
+		);
+
+		$result = $this->db->fetchAllString("exampleCollection/getAllNames");
+		self::assertCount(3, $result);
+		self::assertIsArray($result);
+		foreach($result as $value) {
+			self::assertIsString($value);
+		}
+	}
+
+	public function testFetchFloat() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getHalfByNumber.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT halfNumber FROM test_table WHERE number = ? LIMIT 1"
+		);
+
+		$result2 = $this->db->fetchFloat("exampleCollection/getHalfByNumber", 2);
+		$result3 = $this->db->fetchFloat("exampleCollection/getHalfByNumber", 3);
+		self::assertIsFloat($result2);
+		self::assertIsFloat($result3);
+		self::assertEquals(1, $result2);
+		self::assertEquals(1.5, $result3);
+	}
+
+	public function testFetchAllFloat() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getAllHalves.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT halfNumber FROM test_table"
+		);
+
+		$result = $this->db->fetchAllFloat("exampleCollection/getAllHalves");
+		self::assertCount(3, $result);
+		self::assertIsArray($result);
+		foreach($result as $value) {
+			self::assertIsFloat($value);
+		}
+	}
+
+	public function testFetchDateTime() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getLatestTimestamp.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT timestamp FROM test_table ORDER BY timestamp DESC LIMIT 1"
+		);
+
+		$result = $this->db->fetchDateTime("exampleCollection/getLatestTimestamp");
+		self::assertInstanceOf(DateTime::class, $result);
+	}
+
+	public function testFetchAllDateTime() {
+		$queryCollectionPath = $this->queryBase . "/exampleCollection";
+		$getByNameNumberQueryPath = $queryCollectionPath . "/getAllTimestamps.sql";
+
+		mkdir($queryCollectionPath, 0775, true);
+		file_put_contents(
+			$getByNameNumberQueryPath,
+			"SELECT timestamp FROM test_table ORDER BY timestamp"
+		);
+
+		$result = $this->db->fetchAllDateTime("exampleCollection/getAllTimestamps");
+		self::assertCount(3, $result);
+		self::assertIsArray($result);
+		foreach($result as $value) {
+			self::assertInstanceOf(DateTime::class, $value);
+		}
 	}
 
 	private function settingsSingleton():Settings {
