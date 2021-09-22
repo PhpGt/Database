ef441d0b561358139c7f4baf78464420fe29a880
Greg Bowler <greg.bowler@g105b.com>
2021-09-22 18:26:38 +0100

    wip: refactor execture command

--- src/Cli/ExecuteCommand.php
+++ src/Cli/ExecuteCommand.php
@@ -5,32 +5,16 @@ use Gt\Cli\Argument\ArgumentValueList;
 use Gt\Cli\Command\Command;
 use Gt\Cli\Parameter\NamedParameter;
 use Gt\Cli\Parameter\Parameter;
+use Gt\Config\ConfigFactory;
+use Gt\Database\Connection\Settings;
+use Gt\Database\Migration\Migrator;
 
 class ExecuteCommand extends Command {
 	public function run(ArgumentValueList $arguments = null):void {
-		$forced = false;
-		if(!empty($argv[1])
-			&& ($argv[1] === "--force" || $argv[1] === "-f")) {
-			$forced = true;
-		}
+		$forced = $arguments->contains("force");
 
-		$defaultPath = implode(DIRECTORY_SEPARATOR, [
-			$repoBasePath,
-			"vendor",
-			"phpgt",
-			"webengine",
-		]);
-		foreach(["config.default.ini", "default.ini"] as $defaultFile) {
-			$defaultFilePath = $defaultPath . DIRECTORY_SEPARATOR . $defaultFile;
-
-			if(is_file($defaultFilePath)) {
-				$defaultPath = $defaultFilePath;
-			}
-		}
-
-		if(!is_file($defaultPath)) {
-			$defaultPath = null;
-		}
+		$repoBasePath = $this->getRepoBasePath();
+		$defaultPath = $this->getDefaultPath($repoBasePath);
 
 		$config = ConfigFactory::createForProject($repoBasePath);
 
@@ -51,7 +35,7 @@ class ExecuteCommand extends Command {
 			$config->get("database.driver") ?? 'mysql',
 			$config->get("database.schema"),
 			$config->get("database.host") ?? "localhost",
-			$config->get("database.port") ?? "3306",
+			(int)($config->get("database.port") ?? "3306"),
 			$config->get("database.username"),
 			$config->get("database.password")
 		);
@@ -80,18 +64,62 @@ class ExecuteCommand extends Command {
 	}
 
 	public function getRequiredNamedParameterList():array {
-		// TODO: Implement getRequiredNamedParameterList() method.
+		return [];
 	}
 
 	public function getOptionalNamedParameterList():array {
-		// TODO: Implement getOptionalNamedParameterList() method.
+// TODO: It would be an improvement to allow passing database settings here rather than always require a config.ini
+		return [];
 	}
 
 	public function getRequiredParameterList():array {
-		// TODO: Implement getRequiredParameterList() method.
+		return [];
 	}
 
 	public function getOptionalParameterList():array {
-		// TODO: Implement getOptionalParameterList() method.
+		return [
+			new Parameter(
+				false,
+				"force",
+				"f",
+				"Forcefully drop the current schema and run from migration 1"
+			)
+		];
+	}
+
+	private function getDefaultPath(string $repoBasePath):?string {
+		$defaultPath = implode(DIRECTORY_SEPARATOR, [
+			$repoBasePath,
+			"vendor",
+			"phpgt",
+			"webengine",
+		]);
+		foreach(["config.default.ini", "default.ini"] as $defaultFile) {
+			$defaultFilePath = $defaultPath . DIRECTORY_SEPARATOR . $defaultFile;
+
+			if(is_file($defaultFilePath)) {
+				return $defaultFilePath;
+			}
+		}
+
+		return null;
+	}
+
+	private function getRepoBasePath():string {
+		$repoBasePath = __DIR__;
+		$i = 0;
+
+		do {
+			$repoBasePath = dirname($repoBasePath);
+			$i++;
+
+			if($i > 10) {
+// TODO: Decide if this functionality should be extracted into its own repo,
+// maybe PhpGt/Path? If so, this should throw a PathException.
+				return "";
+			}
+		}
+		while(!is_dir($repoBasePath . DIRECTORY_SEPARATOR . "vendor"));
+		return $repoBasePath;
 	}
 }
